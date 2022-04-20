7a9184edb2e41629b5832acead00e39559a2d5c1
weizhiting <wztlxmnaix@tongji.edu.cn>
2022-04-20 14:29:38 +0800

    Update CMapKS.py

--- ManuscriptCode/CMapKS.py
+++ ManuscriptCode/CMapKS.py
@@ -4,6 +4,7 @@ from multiprocessing import Pool
 from scipy import stats
 from util import RunMultiProcess
 
+### To speed up KS, using broadcast in numpy and pandas to implement the GSEA algorithm from scratch
 def get_ScoreA(ranka,rankb,Genelist,refGene):
     score = ranka / len(Genelist) - rankb / len(refGene)
     return score.round(4)
@@ -12,7 +13,7 @@ def get_ScoreB(ranka,rankb,Genelist,refGene):
     score = rankb / len(refGene) - (ranka - 1) / len(Genelist)
     return score.round(4)
 
-
+### calculate up gene or down gene set score separately
 def calculateScore(Genelist):
     ranka = np.arange(len(Genelist)).reshape((len(Genelist), 1)) + 1
     rankb = Xtr_rank[Genelist].T.values
@@ -23,6 +24,7 @@ def calculateScore(Genelist):
     score = np.where(score_a >= score_b, score_a, -score_b)
     return score
 
+### calculate KS score
 def KS(X):
     upGenelist, dnGenelist, index = X
     up_es = calculateScore(upGenelist)
@@ -30,7 +32,7 @@ def KS(X):
     es = np.where(np.sign(up_es) == np.sign(dn_es), 0, up_es - dn_es)
     es = pd.DataFrame(es.reshape(1, -1), index = [index], columns= Xtr_rank.index)
     return es
-
+#### input reference and query signatures, return KS score matrix
 def runKS(Xtr, Xte, processes=16, num_genes=100):
     global Xtr_rank
     mylist = []; Xtr_rank = Xtr.rank(axis=1, ascending=False, method='first')
